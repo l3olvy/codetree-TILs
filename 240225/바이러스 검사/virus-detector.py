877754127818ab8333281f542b98112490c0473e
l3olvy <qhsl0615@naver.com>
2024-02-25 20:25:42 +0900

    Title: 바이러스 검사, Time: 479ms, Memory: 122MB, Status: Passed - CodeTree

--- 240225/바이러스 검사/virus-detector.py
+++ 240225/바이러스 검사/virus-detector.py
@@ -11,10 +11,10 @@ count = 0
 for i in shop:
     if i > manager:
         i -= manager
-        if i > member:
+        if i % member == 0:
             count += (i // member) + 1
         else:
-            count += 2
+            count += (i // member) + 2
     else:
         count += 1
 
